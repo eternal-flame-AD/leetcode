a96089ad8622c8f41b98d75616c0e0e64e16ec6c
eternal-flame-AD <eternal_flame-AD@protonmail.com>
2018-09-15 09:51:02 +0800

    94-BinaryTreeInorderTraversal

---
+++ problemset/94-BinaryTreeInorderTraversal/bt.go
@@ -0,0 +1,17 @@
+package bt
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	res := inorderTraversal(root.Left)
+	res = append(res, root.Val)
+	res = append(res, inorderTraversal(root.Right)...)
+	return res
+}
